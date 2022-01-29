d17c0bb38030f9a4a8db82437c73f9de249f3d00
riter angel mamani cordova <riter.cordova@gmail.com>
2022-01-29 18:38:02 -0400

    creacion splash y redirect main

--- app/src/main/java/com/desarrollandoapp/testsoyyo/ui/main/MainActivity.kt
+++ app/src/main/java/com/desarrollandoapp/testsoyyo/ui/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.desarrollandoapp.testsoyyo.ui.main
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -7,12 +9,16 @@ import android.os.Looper
 import com.desarrollandoapp.testsoyyo.R
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        fun newInstance(context: Context) : Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            viewModel.openFirstScreen()
-//        }, 2000)
     }
+
 }
--- app/src/main/java/com/desarrollandoapp/testsoyyo/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/desarrollandoapp/testsoyyo/ui/splash/SplashActivity.kt
@@ -1,12 +1,29 @@
 package com.desarrollandoapp.testsoyyo.ui.splash
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.desarrollandoapp.testsoyyo.R
+import com.desarrollandoapp.testsoyyo.ui.main.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goAnimateActivity(MainActivity.newInstance(this))
+        }, 2000)
+    }
+
+    private fun goAnimateActivity(intent: Intent) {
+        startActivity(intent)
+        finish()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
     }
 }
